a27e25b26f61e481fb128d6394125f7b05402ae6
yangaxnkohla <38688053+yangaxnkohla@users.noreply.github.com>
2020-09-19 14:17:04 +0200

    Update firebase.js

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyCcsWauYk2ecx-RH9Klads-_JHNwuzm42g",
-    authDomain: "inspired-forum.firebaseapp.com",
-    databaseURL: "https://inspired-forum.firebaseio.com"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: ""
 };
 firebase.initializeApp(config);
 export const auth = firebase.auth;
